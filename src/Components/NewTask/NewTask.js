43e9b8c7b4cb90aebde5c2da5b4deb99a6d86cd5
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-18 14:20:31 +0200

    ..

--- src/Components/NewTask/NewTask.js
+++ src/Components/NewTask/NewTask.js
@@ -2,11 +2,14 @@ import React, { useRef } from "react";
 import taskForm from "./NewTask.module.css";
 
 function NewTask(props) {
+  const inputVal = useRef();
   return (
-    <div className={taskForm.wrapper}>
-      <input className={taskForm.input} type="text" maxLength="47" />
-      <button className={taskForm.button}>Add</button>
-    </div>
+    <form className={taskForm.wrapper}>
+      <input className={taskForm.input} type="text" maxLength="47" placeholder="What do you want to remember?" ref={inputVal} />
+      <button type="submit" className={taskForm.button}>
+        Add
+      </button>
+    </form>
   );
 }
 
